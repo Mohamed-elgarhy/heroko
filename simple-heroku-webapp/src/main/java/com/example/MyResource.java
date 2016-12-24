c9ad94f6c39403e310823713e5d6962c5a13d74f
MKader3 <Mohamed.EzzElDin@vodafone.com>
2016-12-24 17:24:13 +0200

    22

--- simple-heroku-webapp/src/main/java/com/example/MyResource.java
+++ simple-heroku-webapp/src/main/java/com/example/MyResource.java
@@ -36,6 +36,12 @@ public class MyResource {
         return "Hello, Heroku!";
     }
     
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getPost() {
+        return "Hello, Heroku!";
+    }
+    
     public static double FibonacciRecursive(int n) {
 		if (n == 0)
 			return 0;
@@ -63,7 +69,100 @@ public class MyResource {
 
 		return Response.status(200).entity(result).build();
 	}
+	
+	@POST
+	@Path("Token")
+	@Produces("application/json")
+	public Response getTokenPost() {
+		String result = "d305d55d-dee8-44ff-837f-b080a3f6a4b0";
 
+		return Response.status(200).entity(result).build();
+	}
+
+	
+	
+	@POST
+	@Produces("application/json")
+	@Path("ReverseWords")
+	public String reverseSentensePOST(@Context UriInfo info) {
+
+		/*
+		 * String sentence = info.getQueryParameters().getFirst("sentence");
+		 * StringBuilder sentenceBuilder = new StringBuilder(); StringBuilder
+		 * wordBuilder = new StringBuilder(); char [] charSentence =
+		 * sentence.toCharArray(); boolean newWordFlag = true; for (int i = 0; i
+		 * < charSentence.length; i++) {
+		 * 
+		 * if (charSentence[i] == ' ' ) { if (newWordFlag) { newWordFlag =
+		 * false; wordBuilder.reverse(); sentenceBuilder.append(wordBuilder); }
+		 * 
+		 * 
+		 * System.out.println("space"); sentenceBuilder.append(charSentence[i]);
+		 * continue; } else { if (!newWordFlag) { newWordFlag = true;
+		 * wordBuilder = new StringBuilder(); }
+		 * 
+		 * 
+		 * System.out.println(charSentence[i]);
+		 * wordBuilder.append(charSentence[i]);
+		 * 
+		 * }
+		 * 
+		 * 
+		 * 
+		 * }
+		 */
+
+		String sentence = info.getQueryParameters().getFirst("sentence");
+		StringBuilder sentenceBuilder = new StringBuilder();
+		StringBuilder wordBuilder = new StringBuilder();
+		char[] charSentence = sentence.toCharArray();
+		List<StringBuilder> result = new ArrayList<StringBuilder>();
+		boolean newWordFlag = true;
+		for (int i = 0; i < charSentence.length; i++) {
+
+			if (charSentence[i] == ' ') {
+				if (newWordFlag) {
+					newWordFlag = false;
+					result.add(wordBuilder);
+					wordBuilder = new StringBuilder();
+				}
+
+				// continue;
+			} else {
+				if (!newWordFlag) {
+					newWordFlag = true;
+					result.add(wordBuilder);
+					wordBuilder = new StringBuilder();
+				}
+			}
+
+			wordBuilder.append(charSentence[i]);
+
+		}
+		result.add(wordBuilder);
+
+		for (Iterator<StringBuilder> iterator = result.iterator(); iterator
+				.hasNext();) {
+			StringBuilder stringBuilder = iterator.next();
+			// System.out.println(stringBuilder);
+			sentenceBuilder.append(stringBuilder.reverse());
+
+		}
+
+		/*
+		 * String[] words = sentence.split("\\s+"); StringBuilder builder ;
+		 * StringBuilder result = new StringBuilder(); for (int i = 0; i <
+		 * words.length; i++) { builder = new StringBuilder(words[i]);
+		 * builder.reverse(); System.out.println(builder);
+		 * result.append(builder); result.append(" "); }
+		 */
+
+		return sentenceBuilder.toString();
+	}
+
+	// static int fib[] =
+
+	
 	
 	@GET
 	@Produces("application/json")
@@ -192,12 +291,88 @@ public class MyResource {
 		}
 
 	}
+	
+	@POST
+	@Path("Fibonacci")
+	@Produces("application/json")
+	public Response fibonacciSeriesPOST(@QueryParam("n") String n) {
+		// TODO Auto-generated method stub
+		/*
+		 * JSONObject jsonObject = new JSONObject(); jsonObject.put("f value",
+		 * n); String result = jsonObject.toString(); return
+		 * Response.status(200).entity(result).build();
+		 */
+		String result = "no content";
+		// return "OK";
+
+		int num = 0;
+		try {
+			num = Integer.parseInt(n);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("message", "The request is invalid.");
+			String res = jsonObject.toString();
+			return Response.status(400).entity(res).build();
+		}
+
+		if (num <= 92) {
+			result = fibonacciDynamicProgramming(num) + "";
+			return Response.status(200).entity(result).build();
+			// return result+"";
+		} else {
+			return Response.status(400).entity(result).build();
+		}
+
+	}
 
 	
 	@GET
 	@Path("TriangleType")
 	@Produces("application/json")
 	public Response calculateTriangle(@QueryParam("a") String a,
+			@QueryParam("b") String b, @QueryParam("c") String c) {
+		// TODO Auto-generated method stub
+		/*
+		 * JSONObject jsonObject = new JSONObject(); jsonObject.put("f value",
+		 * n); String result = jsonObject.toString(); return
+		 * Response.status(200).entity(result).build();
+		 */
+		String result = "Error";
+		// return "OK";
+
+		int a_side = 0;
+		int b_side = 0;
+		int c_side = 0;
+		try {
+			a_side = Integer.parseInt(a);
+			b_side = Integer.parseInt(b);
+			c_side = Integer.parseInt(c);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("message", "The request is invalid.");
+			String res = jsonObject.toString();
+			return Response.status(400).entity(res).build();
+		}
+
+		try {
+			result = calculateTriangle(a_side, b_side, c_side);
+			return Response.status(200).entity(result).build();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return Response.status(400).entity(result).build();
+		}
+
+	}
+	
+	@POST
+	@Path("TriangleType")
+	@Produces("application/json")
+	public Response calculateTrianglePOST(@QueryParam("a") String a,
 			@QueryParam("b") String b, @QueryParam("c") String c) {
 		// TODO Auto-generated method stub
 		/*
